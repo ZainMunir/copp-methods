7e5a08be4f1c06b3c9369a1a84c5672f74a9c05a
ZainMunir <zainmunir999@gmail.com>
2023-06-11 11:00:31 +0200

    added some more locals to main

--- docs/alternative-example.js
+++ docs/alternative-example.js
@@ -30,7 +30,7 @@ function load_instructions() {
     for (let i = 0; i < jas.length; i++) {
         let instruction_div = document.createElement("div");
         instruction_div.classList.add("instruction");
-        if (i == 13 || i == 29) {
+        if (i == 16 || i == 32) {
             instruction_div.classList.add(`inst-${i + 1}a`);
             instruction_div.classList.add(`inst-${i + 1}b`);
         } else {
@@ -128,10 +128,13 @@ var jas = [
     ".main",
     ".var",
     "    random",
+    "    zain",
+    "    jack",
+    "    atze",
     ".end-var",
-    "     BIPUSH 0x50",
-    "     DUP",
-    "    ISTORE random",
+    "    BIPUSH 0x50",
+    "    DUP",
+    "    ISTORE jack",
     "    LDC_W objref",
     "    BIPUSH 0x10",
     "    BIPUSH 0x20",
@@ -167,7 +170,7 @@ var jas = [
     ".end-method"
 ]
 
-var order = [4, 8, 9, 10, 11, 12, 13, "14a", 19, 23, 24, 25, 26, 27, 28, 29, "30a", 33, 37, 38, 39, 40, 41, 42, "30b", 31, "14b", 15, 16, 17]
+var order = [4, 11, 12, 13, 14, 15, 16, "17a", 22, 26, 27, 28, 29, 30, 31, 32, "33a", 36, 40, 41, 42, 43, 44, 45, "33b", 34, "17b", 18, 19, 20]
 
 var bin_parts = [{
     hex: "1dea dfad ",
@@ -184,15 +187,15 @@ var bin_parts = [{
 }, {
     hex: "0000 cafe ",
     abbr: "objref - used in INVOKEVIRTUAL",
-    classes: "constant inst-11 inst-24"
+    classes: "constant inst-14 inst-27"
 }, {
     hex: "0000 0012 ",
     abbr: "Location of add(...) method (offset bytes into program text)",
-    classes: "constant inst-14a"
+    classes: "constant inst-17a"
 }, {
     hex: "0000 0028 ",
     abbr: "Location of times_two(...) method (offset bytes into program text",
-    classes: "constant inst-30a"
+    classes: "constant inst-33a"
 }, {
     hex: "0000 0000 ",
     abbr: "Text Origin",
@@ -204,118 +207,118 @@ var bin_parts = [{
 }, {
     hex: "1050 ",
     abbr: "BIPUSH 0x50",
-    classes: "text inst-8"
+    classes: "text inst-11"
 }, {
     hex: "59",
     abbr: "DUP",
-    classes: "text inst-9"
+    classes: "text inst-12"
 },{
-    hex: "36 00",
-    abbr: "ISTORE random",
-    classes: "text inst-10"
+    hex: "36 02",
+    abbr: "ISTORE jack",
+    classes: "text inst-13"
 },  {
     hex: "13 0000 ",
     abbr: "LDC_W objref (Loading a value for INVOKEVIRTUAL)",
-    classes: "text inst-11"
+    classes: "text inst-14"
 }, {
     hex: "1010<br>",
     abbr: "BIPUSH 0x10",
-    classes: "text inst-12"
+    classes: "text inst-15"
 }, {
     hex: "1020 ",
     abbr: "BIPUSH 20",
-    classes: "text inst-13"
+    classes: "text inst-16"
 }, {
     hex: "b600 01",
     abbr: "INVOKEVIRTUAL (0001 is index of constant)",
-    classes: "text inst-14a"
+    classes: "text inst-17a"
 }, {
     hex: "64 ",
     abbr: "ISUB",
-    classes: "text inst-15"
+    classes: "text inst-18"
 }, {
     hex: "fd",
     abbr: "OUT",
-    classes: "text inst-16"
+    classes: "text inst-19"
 }, {
     hex: "ff ",
     abbr: "HALT",
-    classes: "text inst-17"
+    classes: "text inst-20"
 }, {
     hex: "0003 ",
     abbr: "Number of Args for add(...)",
-    classes: "text inst-14a"
+    classes: "text inst-17a"
 }, {
     hex: "0001 ",
     abbr: "Area size / Local Vars for add(...)",
-    classes: "text inst-14a"
+    classes: "text inst-17a"
 }, {
     hex: "1040 ",
     abbr: "BIPUSH 40",
-    classes: "text inst-23"
+    classes: "text inst-26"
 }, {
     hex: "1300 00",
     abbr: "LDC_W objref (Loading a value for INVOKEVIRTUAL)",
-    classes: "text inst-24"
+    classes: "text inst-27"
 }, {
     hex: "15 01",
     abbr: "ILOAD a",
-    classes: "text inst-25"
+    classes: "text inst-28"
 }, {
     hex: "15 02",
     abbr: "ILOAD b",
-    classes: "text inst-26"
+    classes: "text inst-29"
 }, {
     hex: "10 30",
     abbr: "BIPUSH 30",
-    classes: "text inst-27"
+    classes: "text inst-30"
 }, {
     hex: "36 03",
     abbr: "ISTORE c",
-    classes: "text inst-28"
+    classes: "text inst-31"
 }, {
     hex: "60 ",
     abbr: "IADD",
-    classes: "text inst-29"
+    classes: "text inst-32"
 }, {
     hex: "b600 02",
     abbr: "INVOKEVIRTUAL (0002 is index of constant)",
-    classes: "text inst-30a"
+    classes: "text inst-33a"
 }, {
     hex: "ac ",
     abbr: "IRETURN",
-    classes: "text inst-31"
+    classes: "text inst-34"
 }, {
     hex: "0002 ",
     abbr: "Number of Args for add(...)",
-    classes: "text inst-30a"
+    classes: "text inst-33a"
 }, {
     hex: "0001 ",
     abbr: "Area size / Local Vars for add(...)",
-    classes: "text inst-30a"
+    classes: "text inst-33a"
 }, {
     hex: "1501 ",
     abbr: "ILOAD x",
-    classes: "text inst-37"
+    classes: "text inst-40"
 }, {
     hex: "59",
     abbr: "DUP",
-    classes: "text inst-38"
+    classes: "text inst-41"
 }, {
     hex: "59 ",
     abbr: "DUP",
-    classes: "text inst-39"
+    classes: "text inst-42"
 }, {
     hex: "3602 ",
     abbr: "ISTORE y",
-    classes: "text inst-40"
+    classes: "text inst-43"
 }, {
     hex: "60",
     abbr: "IADD",
-    classes: "text inst-41"
+    classes: "text inst-44"
 }, {
     hex: "ac",
     abbr: "IRETURN",
-    classes: "text inst-42"
+    classes: "text inst-45"
 },
 ]
